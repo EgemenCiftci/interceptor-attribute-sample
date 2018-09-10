03d04c9f54a5a9e36f7f5d17f154c223ab3cd4e6
Egemen Çiftci <egemen.ciftci@icrontech.com>
2018-09-10 15:36:56 +0300

    First commit

---
+++ InterceptorAttributeSample/FodyLogAttribute.cs
@@ -0,0 +1,23 @@
+﻿using MethodBoundaryAspect.Fody.Attributes;
+using System;
+
+namespace InterceptorAttributeSample
+{
+    public sealed class FodyLogAttribute : OnMethodBoundaryAspect
+    {
+        public override void OnEntry(MethodExecutionArgs args)
+        {
+            Console.WriteLine("Log: On entry");
+        }
+
+        public override void OnExit(MethodExecutionArgs args)
+        {
+            Console.WriteLine("Log: On exit");
+        }
+
+        public override void OnException(MethodExecutionArgs args)
+        {
+            Console.WriteLine("Log: On exception");
+        }
+    }
+}
---
+++ InterceptorAttributeSample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace InterceptorAttributeSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestMethod();
+
+            Console.ReadKey();
+        }
+
+        [FodyLog]
+        private static void TestMethod()
+        {
+            Console.WriteLine("Test Method");
+
+            //throw new Exception();
+        }
+    }
+}
